96ae63f6ce589a80980522f8506a5886b757983d
Kimmo Salmela <kimmo.salmela@reun.eu>
2025-05-08 17:44:19 +0300

    Update and export OgTemplatePreview component

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore We're re-exporting Astro components from `.ts` file
+
+export { default as OgTemplatePreview } from "./OgTemplatePreview.astro";
