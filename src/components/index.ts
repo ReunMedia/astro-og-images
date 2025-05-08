0b7da7185588938df52624d7537e1d37b69b41af
Kimmo Salmela <kimmo.salmela@reun.eu>
2025-05-08 17:56:16 +0300

    Use @astrojs/check instead of tsc for type checking
    This is done because we now export Astro components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,1 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore We're re-exporting Astro components from `.ts` file
-
 export { default as OgTemplatePreview } from "./OgTemplatePreview.astro";
